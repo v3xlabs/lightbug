fa2a6784703cec870bac67d7424ff44abe8be959
Lucemans <luc@lucemans.nl>
2024-07-12 22:25:43 +0200

    Introduce Plaftorm

---
+++ platform/src/main.rs
@@ -0,0 +1,210 @@
+use display_interface_spi::SPIInterface;
+use embedded_graphics::geometry::{Point, Size};
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    pixelcolor::RgbColor,
+    prelude::*,
+    primitives::{PrimitiveStyleBuilder, Rectangle},
+};
+use esp_idf_hal::delay::Delay;
+use esp_idf_hal::gpio::{AnyOutputPin, PinDriver, Pull};
+use esp_idf_hal::spi::SpiDriver;
+use esp_idf_hal::spi::{self, *};
+use mipidsi::{models, Builder};
+
+use esp_idf_hal::{delay::FreeRtos, peripherals::Peripherals};
+use esp_idf_sys::*;
+
+/**
+ * Pin Mapping
+ * gpio9 LEDs
+ * spi2 - bus 2
+ * gpio4 - data control (display)
+ * gpio5 - display reset (display)
+ *
+ * the device has 4 individually addressable leds in a row on pin 9
+ */
+
+fn main() -> Result<(), EspError> {
+    esp_idf_svc::sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Booting... this is your 5 seconds to unbrick the device Kappa");
+
+    // Important, do not remove, prevents device bricking
+    FreeRtos::delay_ms(5000);
+
+    // Grab peripherals
+    let peripherals = Peripherals::take().unwrap();
+
+    // Temp button setup
+    // let buttons = setup_buttons(peripherals);
+
+    // Initialize NVS
+    // let nvs_partition = EspDefaultNvsPartition::take().unwrap();
+
+    // Initialize system event loop
+    // let sysloop = EspSystemEventLoop::take().unwrap();
+
+    // Either doesnt matter
+    let mut delay = Delay::new_default();
+    // let mut delay = Ets;
+
+    let rst = PinDriver::input_output_od(peripherals.pins.gpio5).unwrap();
+    let dc = PinDriver::input_output_od(peripherals.pins.gpio4).unwrap();
+    let sdo = peripherals.pins.gpio6;
+    let sdi = peripherals.pins.gpio2;
+
+    let driver_config = spi::SpiDriverConfig {
+        dma: Dma::Disabled,
+        ..Default::default()
+    };
+
+    let spi = SpiDriver::new(
+        peripherals.spi2,
+        peripherals.pins.gpio14, // sclk
+        sdo,                     // mosi
+        Some(sdi),               // miso
+        &driver_config,
+    )?;
+
+    let config = spi::SpiConfig {
+        ..Default::default()
+    };
+
+    let spi = SpiDeviceDriver::new(spi, None::<AnyOutputPin>, &config)?;
+
+    let di = SPIInterface::new(spi, dc);
+
+    let mut display = Builder::new(models::ST7789, di)
+        .reset_pin(rst)
+        .init(&mut delay)
+        .unwrap();
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_color(Rgb565::RED)
+        .stroke_width(3)
+        .fill_color(Rgb565::GREEN)
+        .build();
+
+    Rectangle::new(Point::new(0, 0), Size::new(240, 240))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    let mut button1 = PinDriver::input(peripherals.pins.gpio10).unwrap();
+    button1.set_pull(Pull::Up).unwrap();
+
+    let led_pin = peripherals.pins.gpio9;
+    let channel = peripherals.rmt.channel0;
+
+    // let mut ws2812 = Ws2812Esp32Rmt::new(channel, led_pin).unwrap();
+
+    // let mut hue = unsafe { esp_random() } as u8;
+    loop {
+        // let pixels = std::iter::repeat(hsv2rgb(Hsv {
+        //     hue,
+        //     sat: 255,
+        //     val: 8,
+        // }))
+        // .take(25);
+        // ws2812.write_nocopy(pixels).unwrap();
+
+        // check button 1
+        if button1.is_low() {
+            log::info!("Button 1 pressed!");
+        }
+
+        FreeRtos::delay_ms(100);
+
+        // hue = hue.wrapping_add(10);
+    }
+
+    // // Set up Wi-Fi access point
+    // let mut netif = EspNetif::new_with_conf(&NetifConfiguration {
+    //     custom_mac: None,
+    //     description: heapless::String::from_str("hello").unwrap(),
+    //     route_priority: 1,
+    //     key: heapless::String::new(),
+    //     ip_configuration: esp_idf_svc::ipv4::Configuration::Router(RouterConfiguration {
+    //         subnet: Subnet {
+    //             gateway: Ipv4Addr::new(10, 0, 0, 1),
+    //             mask: Ipv4Addr::new(255, 255, 255, 0).try_into().unwrap(),
+    //         },
+    //         ..Default::default()
+    //     }),
+    //     stack: NetifStack::Ap,
+    // })
+    // .unwrap();
+
+    // let mut wifi = EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs_partition))?;
+
+    // wifi.swap_netif_ap(netif).unwrap();
+
+    // let ap_config = AccessPointConfiguration {
+    //     ssid: heapless::String::from_str("V3X-Lightbug").unwrap(),
+    //     password: heapless::String::from_str("thewifipassword").unwrap(),
+    //     channel: 1,
+    //     auth_method: esp_idf_svc::wifi::AuthMethod::WPA2Personal,
+    //     ..Default::default()
+    // };
+
+    // wifi.set_configuration(&Configuration::AccessPoint(ap_config))?;
+    // wifi.start()?;
+
+    // let ip = wifi.ap_netif().get_ip_info().unwrap();
+    // log::info!("WiFi AP IP address: {}", ip.ip);
+
+    // // Set up HTTP server
+    // let configuration = http::server::Configuration {
+    //     ..Default::default()
+    // };
+    // let mut http = EspHttpServer::new(&configuration).unwrap();
+
+    // http.fn_handler("/", http::Method::Get, |request| {
+    //     log::info!("Request incomming from {}", request.uri());
+    //     request
+    //         .into_ok_response()?
+    //         .write_all(b"<html><body>Hello world!</body></html>")
+    //         .map(|_| ())
+    // })
+    // .unwrap();
+
+    // log::info!("WiFi AP and HTTP server are running!");
+
+    // Keep the main function alive
+    // let mut i = 0;
+    // loop {
+    //     FreeRtos::delay_ms(1000);
+    //     i += 1;
+
+    //     if i > 10 {
+    //         log::info!("Reached 10 seconds, http server");
+    //         i = 0;
+    //     }
+    // }
+}
+
+// /**
+//  * Pin Mapping
+//  * gpio10 - button1
+//  * gpio8 - button2
+//  * gpio3 - button3
+//  * gpio2 - button4
+//  *
+//  * button1 is the most right button
+//  * button4 is the most left button
+//  */
+// pub fn setup_buttons(peripherals: Peripherals) {
+//     let mut button1 = PinDriver::input(peripherals.pins.gpio10).unwrap();
+//     button1.set_pull(esp_idf_hal::gpio::Pull::Up).unwrap();
+
+//     let mut button2 = PinDriver::input(peripherals.pins.gpio8).unwrap();
+//     button2.set_pull(esp_idf_hal::gpio::Pull::Up).unwrap();
+
+//     let mut button3 = PinDriver::input(peripherals.pins.gpio3).unwrap();
+//     button3.set_pull(esp_idf_hal::gpio::Pull::Up).unwrap();
+
+//     let mut button4 = PinDriver::input(peripherals.pins.gpio2).unwrap();
+//     button4.set_pull(esp_idf_hal::gpio::Pull::Up).unwrap();
+// }
