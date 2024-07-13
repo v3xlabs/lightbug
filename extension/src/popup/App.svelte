51e3f684267985be3831f9adb8b5615f8289faef
Jonatan Holmgren <jonatan@jontes.page>
2024-07-13 16:27:18 +0200

    Update styles

--- extension/src/popup/App.svelte
+++ extension/src/popup/App.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { writable } from "svelte/store";
     import Portfolio from "./portfolio/Portfolio.svelte";
+    import "../app.css";
 
     const connected = true;
     // intro > choose-device > choose wallet > wallet page
     type pages = "intro" | "choose-device" | "choose-wallet" | "wallet-page";
     const page = writable<pages>("intro");
-    import "../app.css";
+
+        import "../app.css";
 
     let message = "hello";  
     
@@ -16,22 +18,22 @@
     });
 </script>
 
-<main class="p-8 min-w-96">
+<main class="p-8 min-w-96 bg-[#FEF9ED] h-screen flex flex-col">
     {#if $page === "intro"}
-        <h1 class="text-2xl">Welcome to Lightbug</h1>
-        <button
-            on:click={() => {
-                page.set("choose-device");
-            }}>Next</button
-        >
-        <p id="message">{message}</p>
+        <div class="my-auto">
+            <h1 class="text-4xl my-2">Welcome to Lightbug</h1>
+            <button class="px-4 py-2 bg-purple-800 text-white rounded-lg"
+                on:click={() => {
+                    page.set("choose-device");
+                }}>Next</button
+            >
+            <p id="message">{message}</p>
+        </div>
     {:else if $page === "choose-device"}
-        <h1 class="text-2xl">Choose your device</h1>
-        <button
-            on:click={() => {
-                page.set("choose-wallet");
-            }}>Next</button
-        >
+    <div class="my-auto">
+        <h1 class="text-4xl my-2">Please select a device</h1>
+        <p class="text-lg">Your browser serial popup should open</p>
+    </div>
     {:else if $page === "choose-wallet"}
         <h1 class="text-2xl">Choose your wallet</h1>
         <button
