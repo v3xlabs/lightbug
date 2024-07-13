0a8277d1a12d2d0ef72e9786b879fec810b362a1
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-07-13 19:08:08 +0200

    Improve serial manager

--- extension/src/options/App.svelte
+++ extension/src/options/App.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
     let setting: string = "default setting";
+
+    async function pairFireFly() {
+        let port = await navigator.serial.requestPort().catch((err) => {
+            alert("Error: " + err);
+        });
+
+        if (port) {
+            alert("Successfully connected to " + port.getInfo().usbProductId);
+        }
+    }
 </script>
 
 <main>
     <h1>Options Page</h1>
     <p>Current setting: {setting}</p>
+
+    <button on:click={pairFireFly}>Pair FireFly</button>
 </main>
 
 <style>
--- extension/src/popup/SerialManager.ts
+++ extension/src/popup/SerialManager.ts
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 
 const FIREFLY_PRODUCT_ID = 4097;
 const FIREFLY_VENDOR_ID = 12346;
@@ -10,8 +10,16 @@ class SerialManager {
         "disconnected"
     );
 
+    private _reader: ReadableStreamDefaultReader<string> | null = null;
+
     public constructor() {
         console.log("Serial manager init");
+
+        navigator.serial.addEventListener("connect", (ev) => {
+            console.log("Serial port connected", ev);
+            this.connectPort(ev.target as SerialPort);
+        });
+
         this.init();
     }
 
@@ -27,9 +35,7 @@ class SerialManager {
                     portInfo.usbProductId === FIREFLY_PRODUCT_ID &&
                     portInfo.usbVendorId === FIREFLY_VENDOR_ID
                 ) {
-                    this.setPort(port);
-
-                    await this.tryOpenPort();
+                    await this.connectPort(port);
 
                     break;
                 }
@@ -37,25 +43,48 @@ class SerialManager {
         }
     }
 
-    private setPort(port: SerialPort) {
-        this._port = port;
-        this._port.addEventListener("connect", (ev) => {
-            console.log("Serial port connected", ev);
-            this.status.set("connected");
+    private async connectPort(port: SerialPort) {
+        this.setPort(port);
+        let success = await this.tryOpenPort();
 
-            // this._open = true;
-        });
+        if (!success) {
+            throw new Error("Failed to open port");
+        }
+
+        const decoder = new TextDecoderStream();
+
+        this._reader = port.readable!.pipeThrough(decoder).getReader();
+
+        this.initReader();
+    }
+
+    private setPort(port: SerialPort) {
+        const portInfo = port.getInfo();
+        if (portInfo.usbProductId !== FIREFLY_PRODUCT_ID || portInfo.usbVendorId !== FIREFLY_VENDOR_ID) {
+            throw new Error("Invalid port");
+        }
+
+        this._port = port;
         this._port.addEventListener("disconnect", (ev) => {
             console.log("Serial port disconnected", ev);
-            this.status.set("disconnected");
-
-            // this._open = false;
+            this.removePort();
         });
+
+        this.status.set("connected");
+    }
+
+    private removePort() {
+        this._port = null;
+        this.status.set("disconnected");
     }
 
     private async tryOpenPort() {
         if (this._port === null) {
             throw new Error("Serial port not initialized");
+        }
+
+        if (get(this.status) === "connecting") {
+            throw new Error("Serial port is already connecting");
         }
 
         this.status.set("connecting");
@@ -64,9 +93,11 @@ class SerialManager {
             await this._port.open({ baudRate: FIREFLY_BAUD_RATE });
             console.log("Serial port opened");
             this.status.set("connected");
+            return true;
         } catch (err) {
             console.log("Failed to open serial port", err);
             this.status.set("disconnected");
+            return false
         }
     }
 
@@ -81,12 +112,12 @@ class SerialManager {
     public async request() {
         const port = await navigator.serial
             .requestPort({
-                // filters: [
-                //     {
-                //         usbProductId: FIREFLY_PRODUCT_ID,
-                //         usbVendorId: FIREFLY_VENDOR_ID
-                //     }
-                // ]
+                filters: [
+                    {
+                        usbProductId: FIREFLY_PRODUCT_ID,
+                        usbVendorId: FIREFLY_VENDOR_ID
+                    }
+                ]
             })
             .catch((err) => {
                 console.log("Serial port request failed", err);
@@ -94,9 +125,24 @@ class SerialManager {
 
         if (port) {
             console.log("Serial port selected");
-            this.setPort(port);
-            await this.tryOpenPort();
+            await this.connectPort(port);
         }
+    }
+
+    private async initReader() {
+        console.log("Initializing reader");
+        while (this._reader !== null) {
+            const { value, done } = await this._reader.read();
+            if (done) {
+                this._reader = null;
+                console.log("Reader stopped");
+                break;
+            }
+
+            console.log("Received", value);
+        }
+
+        console.log("Reader stopped");
     }
 }
 
