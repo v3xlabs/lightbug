f3e76b6d28f6cd3ffb763e237ca7a25c1caaad61
Lucemans <luc@lucemans.nl>
2024-07-13 15:47:36 +0200

    Patch Icon

--- extension/src/content/inject.ts
+++ extension/src/content/inject.ts
@@ -7,7 +7,8 @@ const providerDetail = {
     info: {
         uuid: "e7552e29-1135-4105-a1fa-b573343821e9",
         name: "LightBug",
-        icon,
+        // icon,
+        icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'/>",
         rdns: "eth.lightbug"
     },
     provider: lightBugProvider
