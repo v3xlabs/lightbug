69d14e6c49959fce0791e075c1481de83afaeebd
Lucemans <luc@lucemans.nl>
2024-07-14 06:24:49 +0200

    qrbump

--- platform/src/main.rs
+++ platform/src/main.rs
@@ -1,5 +1,6 @@
 use display_interface_spi::SPIInterface;
 use embedded_graphics::geometry::{Point, Size};
+use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
 use embedded_graphics::mono_font::ascii::FONT_10X20;
 use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::primitives::StyledDrawable;
@@ -23,6 +24,7 @@ use esp_idf_hal::{delay::FreeRtos, peripherals::Peripherals};
 use esp_idf_sys::*;
 use profont::PROFONT_18_POINT as ProFont18Point;
 use profont::PROFONT_24_POINT as ProFont24Point;
+use qrcode_generator::QrCodeEcc;
 use rand::Rng;
 use smart_leds::hsv::{hsv2rgb, Hsv};
 use smart_leds::RGB8;
@@ -117,6 +119,8 @@ fn main() -> Result<(), EspError> {
     let channel = peripherals.rmt.channel0;
     let mut ws2812 = Ws2812Esp32Rmt::new(channel, led_pin).unwrap();
 
+    draw_qr(&mut display);
+
     loop {
         // check button 1
         if button1.is_low() && button4.is_low() {
@@ -157,6 +161,32 @@ fn main() -> Result<(), EspError> {
     }
 }
 
+fn draw_qr<'a>(
+    display: &mut Display<
+        SPIInterface<
+            SpiDeviceDriver<'a, SpiDriver<'a>>,
+            PinDriver<esp_idf_hal::gpio::Gpio4, esp_idf_hal::gpio::InputOutput>,
+        >,
+        models::ST7789,
+        PinDriver<esp_idf_hal::gpio::Gpio5, esp_idf_hal::gpio::InputOutput>,
+    >,
+) {
+    let result: Vec<Vec<bool>> =
+        qrcode_generator::to_matrix("https://v3x.fyi/s1", QrCodeEcc::Low).unwrap();
+
+    // convert the 2d result to a 1d array, and map true to red and false to black
+    let result = result
+        .iter()
+        .flat_map(|row| {
+            row.iter()
+                .map(|pixel| if *pixel { 0b11101111 } else { 0b0101_0000 })
+        })
+        .collect::<Vec<u8>>();
+    let raw = ImageRawLE::new(&result, result.len() as u32);
+    let image = Image::new(&raw, Point::new(0, 0));
+    image.draw(display).unwrap();
+}
+
 fn draw_unlock_screen<'a>(
     display: &mut Display<
         SPIInterface<
@@ -170,112 +200,112 @@ fn draw_unlock_screen<'a>(
     active_num: &u8,
     pin_progress: &u8,
 ) {
-    if *pin_progress >= 4 {
-        draw_unlocked_screen(display, ws2812);
-        return;
-    }
+    //     if *pin_progress >= 4 {
+    //         draw_unlocked_screen(display, ws2812);
+    //         return;
+    //     }
 
-    display.clear(Rgb565::new(253, 238, 198)).unwrap();
+    //     display.clear(Rgb565::new(253, 238, 198)).unwrap();
 
-    let style = PrimitiveStyleBuilder::new()
-        .stroke_width(1)
-        .fill_color(Rgb565::new(231, 217, 4))
-        .build();
+    //     let style = PrimitiveStyleBuilder::new()
+    //         .stroke_width(1)
+    //         .fill_color(Rgb565::new(231, 217, 4))
+    //         .build();
 
-    Rectangle::new(Point::new(0, 210), Size::new(240, 30))
-        .into_styled(style)
-        .draw(display)
-        .unwrap();
+    //     Rectangle::new(Point::new(0, 210), Size::new(240, 30))
+    //         .into_styled(style)
+    //         .draw(display)
+    //         .unwrap();
 
-    let style = MonoTextStyle::new(&ProFont24Point, Rgb565::BLACK);
-    let mut text = Text::new("Unlock Device", Point::new(120, 80), style);
-    text.text_style.alignment = Alignment::Center;
-    text.draw(display).unwrap();
+    //     let style = MonoTextStyle::new(&ProFont24Point, Rgb565::BLACK);
+    //     let mut text = Text::new("Unlock Device", Point::new(120, 80), style);
+    //     text.text_style.alignment = Alignment::Center;
+    //     text.draw(display).unwrap();
 
-    let rect_style = PrimitiveStyleBuilder::new()
-        .stroke_width(1)
-        .stroke_color(Rgb565::BLACK)
-        .fill_color(Rgb565::BLACK)
-        .build();
-    Rectangle::new(Point::new(108, 200), Size::new(24, 40))
-        .draw_styled(&rect_style, display)
-        .unwrap();
+    //     let rect_style = PrimitiveStyleBuilder::new()
+    //         .stroke_width(1)
+    //         .stroke_color(Rgb565::BLACK)
+    //         .fill_color(Rgb565::BLACK)
+    //         .build();
+    //     Rectangle::new(Point::new(108, 200), Size::new(24, 40))
+    //         .draw_styled(&rect_style, display)
+    //         .unwrap();
 
-    let active_style = MonoTextStyle::new(&ProFont24Point, Rgb565::WHITE);
-    let active_num_str = format!("{}", active_num);
-    let mut active_num_txt = Text::new(&active_num_str, Point::new(120, 230), active_style);
-    active_num_txt.text_style.alignment = Alignment::Center;
-    active_num_txt.draw(display).unwrap();
+    //     let active_style = MonoTextStyle::new(&ProFont24Point, Rgb565::WHITE);
+    //     let active_num_str = format!("{}", active_num);
+    //     let mut active_num_txt = Text::new(&active_num_str, Point::new(120, 230), active_style);
+    //     active_num_txt.text_style.alignment = Alignment::Center;
+    //     active_num_txt.draw(display).unwrap();
 
-    let style = MonoTextStyle::new(&ProFont18Point, Rgb565::BLACK);
+    //     let style = MonoTextStyle::new(&ProFont18Point, Rgb565::BLACK);
 
-    // iterate -5 and +5 and print the numbers
-    for i in -5i8..=5 {
-        if i == 0 {
-            continue;
-        }
-        // map i to the range 0-9
-        let current_num: i32 = {
-            let mut current_num = *active_num as i8 + i;
-            if current_num < 0 {
-                current_num += 10;
-            }
-            current_num % 10
-        }
-        .into();
+    //     // iterate -5 and +5 and print the numbers
+    //     for i in -5i8..=5 {
+    //         if i == 0 {
+    //             continue;
+    //         }
+    //         // map i to the range 0-9
+    //         let current_num: i32 = {
+    //             let mut current_num = *active_num as i8 + i;
+    //             if current_num < 0 {
+    //                 current_num += 10;
+    //             }
+    //             current_num % 10
+    //         }
+    //         .into();
 
-        let current_num_str = format!("{}", current_num);
-        // multiply i by 5 to get the x position
-        let current_num_x: i32 = 120 + (i as i32 * 20);
-        let mut current_num_txt =
-            Text::new(&current_num_str, Point::new(current_num_x, 230), style);
-        current_num_txt.text_style.alignment = Alignment::Center;
-        current_num_txt.draw(display).unwrap();
-    }
+    //         let current_num_str = format!("{}", current_num);
+    //         // multiply i by 5 to get the x position
+    //         let current_num_x: i32 = 120 + (i as i32 * 20);
+    //         let mut current_num_txt =
+    //             Text::new(&current_num_str, Point::new(current_num_x, 230), style);
+    //         current_num_txt.text_style.alignment = Alignment::Center;
+    //         current_num_txt.draw(display).unwrap();
+    //     }
 
-    // Draw Code Progress
-    let code_field_width = 10;
-    let code_field_height = 24;
-    let code_field_spacing = 4;
+    //     // Draw Code Progress
+    //     let code_field_width = 10;
+    //     let code_field_height = 24;
+    //     let code_field_spacing = 4;
 
-    let box_style = PrimitiveStyleBuilder::new()
-        .fill_color(Rgb565::CSS_GRAY)
-        .build();
-    let box_style_2 = PrimitiveStyleBuilder::new()
-        .fill_color(Rgb565::CSS_WHITE)
-        .build();
-    for i in 0..4 {
-        let x = 120 - 2 * (code_field_width + code_field_spacing)
-            + (i * (code_field_width + code_field_spacing));
-        let style = if i < *pin_progress {
-            &box_style
-        } else {
-            &box_style_2
-        };
-        Rectangle::new(
-            Point::new(x as i32, 100),
-            Size::new(code_field_width.into(), code_field_height),
-        )
-        .draw_styled(style, display)
-        .unwrap();
-    }
+    //     let box_style = PrimitiveStyleBuilder::new()
+    //         .fill_color(Rgb565::CSS_GRAY)
+    //         .build();
+    //     let box_style_2 = PrimitiveStyleBuilder::new()
+    //         .fill_color(Rgb565::CSS_WHITE)
+    //         .build();
+    //     for i in 0..4 {
+    //         let x = 120 - 2 * (code_field_width + code_field_spacing)
+    //             + (i * (code_field_width + code_field_spacing));
+    //         let style = if i < *pin_progress {
+    //             &box_style
+    //         } else {
+    //             &box_style_2
+    //         };
+    //         Rectangle::new(
+    //             Point::new(x as i32, 100),
+    //             Size::new(code_field_width.into(), code_field_height),
+    //         )
+    //         .draw_styled(style, display)
+    //         .unwrap();
+    //     }
 
-    // Update LEDs
-    let off = hsv2rgb(Hsv {
-        hue: 0,
-        sat: 0,
-        val: 0,
-    });
-    let green = hsv2rgb(Hsv {
-        hue: 120,
-        sat: 255,
-        val: 50,
-    });
-    // let pixels = vec![green, off, off, green];
-    // let pixels = std::iter::repeat(green).take(4);
-    let pixels = vec![green, off, off, green];
+    //     // Update LEDs
+    //     let off = hsv2rgb(Hsv {
+    //         hue: 0,
+    //         sat: 0,
+    //         val: 0,
+    //     });
+    //     let green = hsv2rgb(Hsv {
+    //         hue: 120,
+    //         sat: 255,
+    //         val: 50,
+    //     });
+    //     // let pixels = vec![green, off, off, green];
+    //     // let pixels = std::iter::repeat(green).take(4);
+    //     let pixels = vec![green, off, off, green];
 
-    ws2812.write_nocopy(pixels).unwrap();
+    //     ws2812.write_nocopy(pixels).unwrap();
 }
 
 fn draw_unlocked_screen<'a>(
@@ -289,20 +319,20 @@ fn draw_unlocked_screen<'a>(
     >,
     ws2812: &mut Ws2812Esp32Rmt,
 ) {
-    display.clear(Rgb565::new(253, 238, 198)).unwrap();
+    //     display.clear(Rgb565::new(253, 238, 198)).unwrap();
 
-    let style = MonoTextStyle::new(&ProFont24Point, Rgb565::BLACK);
-    let mut text = Text::new("Welcome!", Point::new(120, 80), style);
-    text.text_style.alignment = Alignment::Center;
-    text.draw(display).unwrap();
+    //     let style = MonoTextStyle::new(&ProFont24Point, Rgb565::BLACK);
+    //     let mut text = Text::new("Welcome!", Point::new(120, 80), style);
+    //     text.text_style.alignment = Alignment::Center;
+    //     text.draw(display).unwrap();
 
-    // Update LEDs
-    let off = hsv2rgb(Hsv {
-        hue: 0,
-        sat: 0,
-        val: 0,
-    });
-    let green = RGB8::new(0, 255, 0);
-    let pixels = vec![green, green, green, green];
-    ws2812.write_nocopy(pixels).unwrap();
+    //     // Update LEDs
+    //     let off = hsv2rgb(Hsv {
+    //         hue: 0,
+    //         sat: 0,
+    //         val: 0,
+    //     });
+    //     let green = RGB8::new(0, 255, 0);
+    //     let pixels = vec![green, green, green, green];
+    //     ws2812.write_nocopy(pixels).unwrap();
 }
