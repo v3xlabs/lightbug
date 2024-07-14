90bad849aaa6bbf414c4837b477025e2548f280d
Jonatan Holmgren <jonatan@jontes.page>
2024-07-14 08:50:01 +0200

    better transaction list

--- extension/src/popup/portfolio/TransactionList.svelte
+++ extension/src/popup/portfolio/TransactionList.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { formatAddress } from "ens-tools";
-    import { formatEther } from "viem";
+    import { formatTransaction } from "viem";
     import { activeWallet } from '../DeviceConnector';
     import { pop } from 'svelte-spa-router';
 
@@ -10,6 +10,14 @@
         );
         return await response.json();
     };
+
+    const mode = (activeWallet:string, txt_from:string) => {
+        if (activeWallet == txt_from) {
+            return 'Sent';
+        } else {
+            return 'Received';
+        }
+    }
 </script>
 <button class="" on:click={pop}>Back</button>
 
@@ -18,9 +26,13 @@
 
     <ul>
         {#each fetchData.items as transaction}
+        <a target="_blank" href={`https://eth.blockscout.com/tx/${transaction.hash}`}>
             <li class="border border-neutral-400 p-2 my-2 rounded-lg w-full">
-                {formatAddress(transaction.from?.ens_name || transaction.from?.hash || '0x0')} - {formatAddress(transaction.to?.ens_name || transaction.to?.hash || '0x0')} - {formatEther(transaction.value)}
+                <!-- {formatAddress(transaction.from?.ens_name || transaction.from?.hash || '0x0')} - {formatAddress(transaction.to?.ens_name || transaction.to?.hash || '0x0')} - {formatEther(transaction.value)} -->
+                <div class="text-lg">{mode($activeWallet, transaction.from) === "Sent" ? "Sent yo " : "Received from "} {mode($activeWallet, transaction.from) === "Sent" ? formatAddress(transaction.to.hash) : formatAddress(transaction.from.hash)}</div>
+                <div class="text-md">{transaction.hash.slice(0,6)}...{transaction.hash.slice(-4)}</div>
             </li>
+        </a>
         {/each}
     </ul>
 {/await}
